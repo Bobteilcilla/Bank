9485a519250a939f9234a019ad5f6bc857bf386f
Erica Gonzalez Gutierrez <erikagonzgut@hotmail.com>
2026-03-05 09:38:03 +0100

    Initial Maven project setup with clean architecture structure

---
+++ src/main/java/com/erika/model/Money.java
@@ -0,0 +1,4 @@
+package com.erika.model;
+
+public class Money {
+}
---
+++ src/test/java/com/erika/Bank/TestExample.java
@@ -0,0 +1,12 @@
+package com.erika.Bank;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class TestExample {
+
+    @Test
+    void testSimpleMath() {
+        assertEquals(4, 2 + 2);
+    }
+}
