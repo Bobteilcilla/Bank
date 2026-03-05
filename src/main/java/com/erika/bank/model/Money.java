fac23265e91fc67442b284c3ded6fd817434ee28
Erica Gonzalez Gutierrez <erikagonzgut@hotmail.com>
2026-03-05 13:28:27 +0100

    Added some extra helper methods and overrides like hashCode and equals. Fixed some minor error on the exception text on the subtract method

--- src/main/java/com/erika/bank/model/Money.java
+++ src/main/java/com/erika/bank/model/Money.java
@@ -34,7 +34,7 @@ public class Money {
 
     public Money subtract(Money other){
         if (other == null){
-            throw new IllegalArgumentException("Money to add cannot be null");
+            throw new IllegalArgumentException("Money to subtract cannot be null");
         }
         return new Money(this.amount.subtract(other.amount));
     }
@@ -43,5 +43,47 @@ public class Money {
     public String toString(){
         return amount.toString();
     }
+
+    public int compareTo(Money other) {
+        if (other == null ) throw new IllegalArgumentException("Other money cannot be null");
+        return this.amount.compareTo(other.amount);
+    }
+
+    public boolean isGreaterThan(Money other) {
+        if (other == null ) {
+            throw new IllegalArgumentException("Other money cannot be null");
+        }
+        return this.amount.compareTo(other.amount) > 0;
+    }
+
+    public boolean isLessThan(Money other) {
+        if (other == null ) {
+            throw new IllegalArgumentException("Other money cannot be null");
+        }
+        return this.amount.compareTo(other.amount) < 0;
+    }
+
+    public boolean isGreaterOrEqual(Money other) {
+        if (other == null ) {
+            throw new IllegalArgumentException("Other money cannot be null");
+        }
+        return this.amount.compareTo(other.amount) >= 0;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(this == o) return true;
+        if(!(o instanceof Money other)) return false;
+        //compareTo ignore scale differences
+        return this.amount.compareTo(other.amount) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        //normalize scale for consistent hashing
+        return this.amount.stripTrailingZeros().hashCode();
+    }
+
+
 
 }
