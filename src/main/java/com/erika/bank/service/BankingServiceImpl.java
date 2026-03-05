4e32e0b6ad2270ea004ce7a7aff8a2189b30f45f
Erica Gonzalez Gutierrez <erikagonzgut@hotmail.com>
2026-03-05 13:13:49 +0100

    Create BankingServiceImpl simple version, creation of the Accountrepository and the first method createAccount

--- src/main/java/com/erika/bank/service/BankingServiceImpl.java
+++ src/main/java/com/erika/bank/service/BankingServiceImpl.java
@@ -1,4 +1,47 @@
 package com.erika.bank.service;
 
-public class BankingServiceImpl {
+import com.erika.bank.model.Money;
+import com.erika.bank.model.Transaction;
+import com.erika.bank.model.Account;
+import com.erika.bank.repository.AccountRepository;
+import com.erika.bank.repository.InMemoryAccountRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public class BankingServiceImpl implements BankingService {
+
+    private final AccountRepository repo;
+
+    public BankingServiceImpl(AccountRepository repo){
+        this.repo = repo;
+    }
+
+    @Override
+    public String createAccount(String ownerName, Money initialDeposit) {
+        String accountId = UUID.randomUUID().toString();
+        Account account = new Account(accountId, ownerName, initialDeposit);
+        repo.save(account);
+        return accountId;
+    }
+
+    @Override
+    public Money getBalance(String accountId) {
+        return null;
+    }
+
+    @Override
+    public void deposit(String accountId, Money amount) {
+
+    }
+
+    @Override
+    public void withdraw(String accountId, Money amount) {
+
+    }
+
+    @Override
+    public List<Transaction> getTransactions(String accountId) {
+        return List.of();
+    }
 }
