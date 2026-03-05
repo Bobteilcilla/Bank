8698074515ec153232e7593d9146761c6266e018
Erica Gonzalez Gutierrez <erikagonzgut@hotmail.com>
2026-03-05 11:50:35 +0100

    First new test TestAccount class

--- src/test/java/com/erika/bank/model/AccountTest.java
+++ src/test/java/com/erika/bank/model/AccountTest.java
@@ -2,12 +2,31 @@ package com.erika.bank.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AccountTest {
 
     @Test
     void deposit_increasing_balance_and_records_transaction() {
-        Account a = new Account("A1","Erica", Money.of("100.00"));
+        Account account = new Account(
+                "A1",
+                "Erica",
+                Money.of("100.00")
+        );
 
+        Transaction tx = new Transaction(
+                "123",
+                TransactionType.DEPOSIT,
+                "A1",
+                Money.of("20.00"),
+                Instant.now()
+        );
+
+        account.deposit(Money.of("20.00"), tx);
+        assertEquals("120.00", account.getBalance().toString());
+        assertEquals(1, account.getTransactions().size());
 
     }
 }
