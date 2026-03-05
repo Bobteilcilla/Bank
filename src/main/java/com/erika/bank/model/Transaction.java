09ddeaa694116001cd6a877ca15072a4f77a818c
Erica Gonzalez Gutierrez <erikagonzgut@hotmail.com>
2026-03-05 10:50:06 +0100

    Add Transaction class with the parameters and the constructor

---
+++ src/main/java/com/erika/bank/model/Transaction.java
@@ -0,0 +1,27 @@
+package com.erika.bank.model;
+
+import java.time.Instant;
+import java.util.Objects;
+
+public class Transaction {
+    private final String id;
+    private final TransactionType type;
+    private final String accountId;
+    private final Money amount;
+    private final Instant timestamp;
+
+    public Transaction(String id, TransactionType type, String accountId, Money amount, Instant timestamp) {
+       if(id == null || id.isBlank()){
+           throw new IllegalArgumentException("The Transaction id cannot be null");
+       }
+       if(accountId == null || accountId.isBlank()){
+           throw new IllegalArgumentException("The Account id cannot be null");
+       }
+
+        this.id = id;
+        this.type = Objects.requireNonNull(type);;
+        this.accountId = accountId;
+        this.amount = Objects.requireNonNull(amount);
+        this.timestamp = Objects.requireNonNull(timestamp);
+    }
+}
