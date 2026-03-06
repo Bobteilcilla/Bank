b3167568cb5f6684b9472a8ee3b7157b4e386352
Erica Gonzalez Gutierrez <erikagonzgut@hotmail.com>
2026-03-06 13:36:51 +0100

    Add new methods to handle get transations under different circumstances

--- src/main/java/com/erika/bank/service/BankingService.java
+++ src/main/java/com/erika/bank/service/BankingService.java
@@ -2,7 +2,9 @@ package com.erika.bank.service;
 
 import com.erika.bank.model.Money;
 import com.erika.bank.model.Transaction;
+import com.erika.bank.model.TransactionType;
 
+import java.time.Instant;
 import java.util.*;
 
 public interface BankingService {
@@ -18,5 +20,11 @@ public interface BankingService {
     void transfer(String fromAccountId, String toAccountId, Money amount);
 
     List<Transaction> getTransactions(String accountId);
+
+    List<Transaction> getTransactionsPerType(String accountId, TransactionType expectedType);
+
+    List<Transaction> getTransactionsBetween(String accountId, Instant fromTime, Instant toTime);
+
+    List<Transaction> getTransactionsFromLastDays(String accountId, int days);
 
 }
