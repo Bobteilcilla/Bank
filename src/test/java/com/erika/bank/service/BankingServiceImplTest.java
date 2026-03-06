210ff0d26de96dd412c2235b7e036f1e737962e9
Erica Gonzalez Gutierrez <erikagonzgut@hotmail.com>
2026-03-06 10:16:32 +0100

    Add new BankingService Test methods

--- src/test/java/com/erika/bank/service/BankingServiceImplTest.java
+++ src/test/java/com/erika/bank/service/BankingServiceImplTest.java
@@ -1,6 +1,6 @@
 package com.erika.bank.service;
 
-import com.erika.bank.model.Account;
+
 import com.erika.bank.model.Money;
 import com.erika.bank.model.TransactionType;
 import com.erika.bank.repository.InMemoryAccountRepository;
@@ -11,26 +11,66 @@ import static org.junit.jupiter.api.Assertions.*;
 public class BankingServiceImplTest {
 
     @Test
-    void createAccount_setId_setsInitialBalance(){
+    void create_account_sets_id_and_initial_balance(){
         BankingService service= new BankingServiceImpl(new InMemoryAccountRepository());
 
         String id = service.createAccount("Erika", Money.of("0.00"));
 
         assertNotNull(id);
         assertFalse(id.isBlank());
-        assertEquals("0.00", service.getBalance(id).toString());
+        assertEquals(Money.of("0.00"), service.getBalance(id));
         assertEquals(0, service.getTransactions(id).size());
     }
 
     @Test
-    void createAccount_with_initial_deposit_records_transaction() {
+    void create_account_with_initial_deposit_records_transaction() {
 
         BankingService service = new BankingServiceImpl(new InMemoryAccountRepository());
 
         String id = service.createAccount("Erika", Money.of("100.00"));
 
-        assertEquals("100.00", service.getBalance(id).toString());
+        assertEquals(Money.of("100.00"), service.getBalance(id));
         assertEquals(1, service.getTransactions(id).size());
+    }
+
+    @Test
+    void deposit_records_correct_transaction_with_no_funds() {
+        BankingService service = new BankingServiceImpl(new InMemoryAccountRepository());
+
+        String id = service.createAccount("Maria", Money.of("0.00"));
+
+        service.deposit(id, Money.of("50.00"));
+
+        var transactions = service.getTransactions(id);
+
+        assertEquals(1, transactions.size());
+
+        var tx = transactions.get(0);
+
+        assertEquals(id, tx.getAccountId());
+        assertEquals(TransactionType.DEPOSIT, tx.getType());
+        assertEquals(Money.of("50.00"), tx.getAmount());
+        assertEquals(Money.of("50.00"), service.getBalance(id));
+    }
+
+    @Test
+    void deposit_records_correct_transaction_with_funds(){
+
+        BankingService service = new BankingServiceImpl(new InMemoryAccountRepository());
+
+        String id = service.createAccount("Maria", Money.of("100.00"));
+
+        service.deposit(id, Money.of("50.00"));
+
+        var transactions = service.getTransactions(id);
+
+        assertEquals(2, transactions.size());
+
+        var tx = transactions.get(1); // second transaction
+
+        assertEquals(id, tx.getAccountId());
+        assertEquals(TransactionType.DEPOSIT, tx.getType());
+        assertEquals(Money.of("50.00"), tx.getAmount());
     }
 
     @Test
@@ -41,7 +81,7 @@ public class BankingServiceImplTest {
 
         service.deposit(id, Money.of("50.00"));
 
-        assertEquals("50.00", service.getBalance(id).toString());
+        assertEquals(Money.of("50.00"), service.getBalance(id));
         assertEquals(1, service.getTransactions(id).size());
 
     }
@@ -54,7 +94,7 @@ public class BankingServiceImplTest {
 
         service.withdraw(id, Money.of("50.00"));
 
-        assertEquals("50.00", service.getBalance(id).toString());
+        assertEquals(Money.of("50.00"), service.getBalance(id));
         assertEquals(2, service.getTransactions(id).size());
     }
 
@@ -74,5 +114,80 @@ public class BankingServiceImplTest {
 
         assertThrows(IllegalArgumentException.class, () -> service.deposit(id, Money.of("0.00")));
         assertThrows(IllegalArgumentException.class, () -> service.deposit(id, Money.of("-1.00")));
+    }
+
+    @Test
+    void transfer_to_same_account_throws(){
+        BankingService service = new BankingServiceImpl(new InMemoryAccountRepository());
+        String accountId = service.createAccount("Erika", Money.of("20.00"));
+
+        assertThrows(IllegalArgumentException.class, () -> service.transfer(accountId, accountId, Money.of("10.00")));
+    }
+
+    @Test
+    void transfer_more_than_balance_throws(){
+        BankingService service = new BankingServiceImpl(new InMemoryAccountRepository());
+        String fromAccountId = service.createAccount("Erika", Money.of("100.00"));
+        String toAccountId = service.createAccount("Manolo", Money.of("50.00"));
+
+        assertThrows(IllegalArgumentException.class, () -> service.transfer(fromAccountId, toAccountId, Money.of("200.00")));
+    }
+
+    @Test
+    void transfer_moves_money_between_accounts_and_records_transactions(){
+        BankingService service = new BankingServiceImpl(new InMemoryAccountRepository());
+        String fromAccountId = service.createAccount("Erika", Money.of("100.00"));
+        String toAccountId = service.createAccount("Manolo", Money.of("0.00"));
+
+        service.transfer(fromAccountId, toAccountId, Money.of("20.00"));
+
+        assertEquals(Money.of("20.00"), service.getBalance(toAccountId));
+        assertEquals(Money.of("80.00"), service.getBalance(fromAccountId));
+
+        var fromTransactions = service.getTransactions(fromAccountId);
+        var toTransactions = service.getTransactions(toAccountId);
+
+        assertEquals(2, fromTransactions.size());
+        assertEquals(1, toTransactions.size());
+
+        var withdrawTx = fromTransactions.get(1);
+        var depositTx = toTransactions.get(0);
+
+        assertEquals(TransactionType.WITHDRAW, withdrawTx.getType());
+        assertEquals(fromAccountId, withdrawTx.getAccountId());
+        assertEquals(Money.of("20.00"), withdrawTx.getAmount());
+
+        assertEquals(TransactionType.DEPOSIT, depositTx.getType());
+        assertEquals(toAccountId, depositTx.getAccountId());
+        assertEquals(Money.of("20.00"), depositTx.getAmount());
+
+    }
+
+    @Test
+    void withdraw_zero_or_negative_throws(){
+
+        BankingService service = new BankingServiceImpl(new InMemoryAccountRepository());
+
+        String id = service.createAccount("Erika", Money.of("40.00"));
+
+        assertThrows(IllegalArgumentException.class, () -> service.withdraw(id, Money.of("-10.00")));
+        assertThrows(IllegalArgumentException.class, () -> service.withdraw(id, Money.of("0.00")));
+
+    }
+
+    @Test
+    void get_balance_for_unknown_account_throws(){
+        BankingService service = new BankingServiceImpl(new InMemoryAccountRepository());
+
+        assertThrows(IllegalArgumentException.class,
+                () -> service.getBalance("unknown-account"));
+    }
+
+    @Test
+    void deposit_to_unknown_account_throws() {
+        BankingService service = new BankingServiceImpl(new InMemoryAccountRepository());
+
+        assertThrows(IllegalArgumentException.class,
+                () -> service.deposit("unknown-account", Money.of("10.00")));
     }
 }
