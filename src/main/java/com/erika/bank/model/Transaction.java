573e51a8753b37ce105d775dce0beb21bfb7af37
Erica Gonzalez Gutierrez <erikagonzgut@hotmail.com>
2026-03-06 16:02:47 +0100

    Add description parameter to Transaction

--- src/main/java/com/erika/bank/model/Transaction.java
+++ src/main/java/com/erika/bank/model/Transaction.java
@@ -9,31 +9,38 @@ public class Transaction {
     private final String accountId;
     private final Money amount;
     private final Instant timestamp;
+    private final String description;
 
-    public Transaction(String id, TransactionType type, String accountId, Money amount, Instant timestamp) {
-       if(id == null || id.isBlank()){
-           throw new IllegalArgumentException("The Transaction id cannot be null");
-       }
-       if(accountId == null || accountId.isBlank()){
-           throw new IllegalArgumentException("The Account id cannot be null");
-       }
+    public Transaction(String id, TransactionType type, String accountId, Money amount, Instant timestamp, String description) {
+
+        if (id == null || id.isBlank()) {
+            throw new IllegalArgumentException("The Transaction id cannot be null");
+        }
+        if (accountId == null || accountId.isBlank()) {
+            throw new IllegalArgumentException("The Account id cannot be null");
+        }
 
         this.id = id;
-        this.type = Objects.requireNonNull(type);;
+        this.type = Objects.requireNonNull(type);
+        ;
         this.accountId = accountId;
         this.amount = Objects.requireNonNull(amount);
         this.timestamp = Objects.requireNonNull(timestamp);
+        this.description = description;
     }
 
     public String getId() {
+
         return id;
     }
 
     public TransactionType getType() {
+
         return type;
     }
 
     public String getAccountId() {
+
         return accountId;
     }
 
@@ -42,6 +49,17 @@ public class Transaction {
     }
 
     public Instant getTimestamp() {
+
         return timestamp;
     }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return timestamp + " | " + type + " | " + amount + " | " + description;
+    }
 }
+
