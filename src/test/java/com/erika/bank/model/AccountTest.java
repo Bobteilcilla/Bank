32c877e3142b8e476381aab9239448084742eb38
Erica Gonzalez Gutierrez <erikagonzgut@hotmail.com>
2026-03-10 09:21:17 +0100

    Add new test cases for Account model class, involving withdrawl and deposit with wrong and null types

--- src/test/java/com/erika/bank/model/AccountTest.java
+++ src/test/java/com/erika/bank/model/AccountTest.java
@@ -43,6 +43,120 @@ public class AccountTest {
     }
 
     @Test
+    void deposit_with_null_transaction_throws(){
+
+        Account account = new Account(
+                "A1",
+                "Erica",
+                Money.of("100.00")
+        );
+
+       assertThrows(NullPointerException.class, () -> account.deposit(Money.of("20.00"), null));
+
+    }
+
+    @Test
+    void deposit_with_wrong_transaction_type_throws(){
+
+        Account account = new Account(
+                "A1",
+                "Erica",
+                Money.of("100.00")
+        );
+
+        Transaction tx = new Transaction(
+                "123",
+                TransactionType.WITHDRAW,
+                "A1",
+                Money.of("20.00"),
+                Instant.now(),
+                "Deposit to account A1"
+        );
+
+        assertThrows(IllegalArgumentException.class, () -> account.deposit(Money.of("20.00"), tx));
+
+    }
+
+    @Test
+    void deposit_with_transaction_for_different_account_throws(){
+
+        Account account = new Account(
+                "A1",
+                "Erica",
+                Money.of("100.00")
+        );
+
+        Transaction tx = new Transaction(
+                "123",
+                TransactionType.DEPOSIT,
+                "A5",
+                Money.of("20.00"),
+                Instant.now(),
+                "Deposit to account A1"
+        );
+
+        assertThrows(IllegalArgumentException.class, () -> account.deposit(Money.of("20.00"), tx));
+
+    }
+
+    @Test
+    void withdraw_with_null_transaction_throws(){
+
+        Account account = new Account(
+                "A1",
+                "Erica",
+                Money.of("100.00")
+        );
+
+        assertThrows(NullPointerException.class, () -> account.withdraw(Money.of("20.00"), null));
+
+    }
+
+    @Test
+    void withdraw_with_wrong_transaction_type_throws(){
+
+        Account account = new Account(
+                "A1",
+                "Erica",
+                Money.of("100.00")
+        );
+
+        Transaction tx = new Transaction(
+                "123",
+                TransactionType.DEPOSIT,
+                "A1",
+                Money.of("20.00"),
+                Instant.now(),
+                "Deposit to account A1"
+        );
+
+        assertThrows(IllegalArgumentException.class, () -> account.withdraw(Money.of("20.00"), tx));
+
+    }
+
+    @Test
+    void withdraw_with_transaction_for_different_account_throws(){
+
+        Account account = new Account(
+                "A1",
+                "Erica",
+                Money.of("100.00")
+        );
+
+        Transaction tx = new Transaction(
+                "123",
+                TransactionType.WITHDRAW,
+                "A5",
+                Money.of("20.00"),
+                Instant.now(),
+                "Deposit to account A1"
+        );
+
+        assertThrows(IllegalArgumentException.class, () -> account.withdraw(Money.of("20.00"), tx));
+
+    }
+
+    @Test
     void deposit_increasing_balance_and_records_transaction() {
         Account account = new Account(
                 "A1",
