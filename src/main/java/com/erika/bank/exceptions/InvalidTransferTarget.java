5e90579b9fea28d7a0f6c152b8634f879c320c66
Erica Gonzalez Gutierrez <erikagonzgut@hotmail.com>
2026-03-10 08:26:13 +0100

    Add the accountId on the InvalidTransferTarget message

--- src/main/java/com/erika/bank/exceptions/InvalidTransferTarget.java
+++ src/main/java/com/erika/bank/exceptions/InvalidTransferTarget.java
@@ -3,6 +3,6 @@ package com.erika.bank.exceptions;
 public class InvalidTransferTarget extends BankingException{
 
     public InvalidTransferTarget(String accountId){
-        super("The target account cannot be the same as the initiating one");
+        super("The target account cannot be the same as the initiating one" + accountId);
     }
 }
