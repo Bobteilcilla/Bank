22d2cacee717112c5fa4b94e6bf3b6e9db53872d
Erica Gonzalez Gutierrez <erikagonzgut@hotmail.com>
2026-03-10 08:30:12 +0100

    Remove unused import

--- src/main/java/com/erika/bank/exceptions/InvalidTimeRangeException.java
+++ src/main/java/com/erika/bank/exceptions/InvalidTimeRangeException.java
@@ -1,7 +1,5 @@
 package com.erika.bank.exceptions;
 
-import java.time.Instant;
-
 public class InvalidTimeRangeException extends BankingException{
 
     public InvalidTimeRangeException(String time){
