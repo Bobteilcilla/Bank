2d2dbab91decf4bd214236d0800f42290cbadc81
Erica Gonzalez Gutierrez <erikagonzgut@hotmail.com>
2026-03-10 08:29:45 +0100

    Remove the unused import

--- src/main/java/com/erika/bank/exceptions/InvalidAmountException.java
+++ src/main/java/com/erika/bank/exceptions/InvalidAmountException.java
@@ -1,7 +1,5 @@
 package com.erika.bank.exceptions;
 
-import com.erika.bank.model.Money;
-
 public class InvalidAmountException extends BankingException{
 
     public InvalidAmountException(String operation){
