a91b3695c088285eea59b081f004a86e080ee963
Erica Gonzalez Gutierrez <erikagonzgut@hotmail.com>
2026-03-10 08:30:53 +0100

    Change the name of the class with Exception at the end to make it consistent with the res

--- src/main/java/com/erika/bank/exceptions/InvalidTransferTargetException.java
+++ src/main/java/com/erika/bank/exceptions/InvalidTransferTargetException.java
@@ -1,8 +1,8 @@
 package com.erika.bank.exceptions;
 
-public class InvalidTransferTarget extends BankingException{
+public class InvalidTransferTargetException extends BankingException{
 
-    public InvalidTransferTarget(String accountId){
+    public InvalidTransferTargetException(String accountId){
         super("The target account cannot be the same as the initiating one" + accountId);
     }
 }
